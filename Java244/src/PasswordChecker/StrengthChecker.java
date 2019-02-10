3bc2aaa55e6f42890102f13b1ac74df678400e61
Noah <nbaertsch@github.com>
2019-02-10 16:40:00 -0500

    finished passwordChecker

--- Java244/src/PasswordChecker/StrengthChecker.java
+++ Java244/src/PasswordChecker/StrengthChecker.java
@@ -1,42 +1,84 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package PasswordChecker;
 
 /**
- *
+ * Used to test a password against a given String of required characters/symbols
+ * 
  * @author noah.baertsch
  */
 public class StrengthChecker {
     
-    private final char[] reqChars;
+    private char[] reqChars;
     private int minLength;
     private int maxLength;
+    private String[][] feedback; 
     
+    /**
+     * Builds a new StrengthChecker object, and instantiates a new String[][] for validation feedback
+     * 
+     * @param reqChars String of required characters/symbols
+     */
     public StrengthChecker(String reqChars){
         this.reqChars = reqChars.toCharArray();
+        this.feedback = new String[(this.reqChars.length + 2)][2];
+        
+        int i=0;
+        while(i < this.reqChars.length){
+            this.feedback[i][0] = Character.toString(this.reqChars[i]);
+            i++;
+        }
+        
+        //minLength
+        this.feedback[i][0] = "minLength";
+        i++;
+        
+        //maxLength
+        this.feedback[i][0] = "maxLength";
     }
     
+    /**
+     * Checks a given String against the requirements
+     * Builds onto the feedback String[][] for granular verification
+     * 
+     * @param pass the password to test
+     * @return boolean representing whether this password passes the requirements
+     * @throws ObjectMisconfiguredException thrown if the StringChecker is misconfigured
+     */
     public boolean checkPassword(String pass) throws ObjectMisconfiguredException{
-        if(minLength <= 0 || maxLength <= 1 || reqChars.length <= 1){
+        boolean passed = true;
+        if(minLength <= 0 || maxLength <= 1 || reqChars.length < 1 || minLength > maxLength){
             throw new ObjectMisconfiguredException();
         }
         
-        if(pass.length()<this.minLength || pass.length()>this.maxLength){
-            return false;
+        if(pass.length()<this.minLength){
+            this.feedback[(this.feedback.length-2)][1] = "failed";
+            passed = false;
+        } else{
+            this.feedback[(this.feedback.length-2)][1] = "passed";
         }
         
-        for(char c : this.reqChars){
-            if(!pass.contains(Character.toString(c))){
-                return false;
+        if(pass.length()>this.maxLength){
+            this.feedback[(this.feedback.length-1)][1] = "failed";
+            passed = false;
+        } else{
+            this.feedback[(this.feedback.length-1)][1] = "passed";
+        }
+        
+        for(int i=0; i<this.feedback.length-2; i++){
+            if(!pass.contains(this.feedback[i][0])){
+                this.feedback[i][1] = "failed";
+                passed = false;
+            } else{
+                this.feedback[i][1] = "passed";
             }
         }
         
-        return true;
+        return passed;
     }
-
+    
+    /**
+     * 
+     * @return required characters/symbols
+     */
     public char[] getReqChars(){
         return this.reqChars;
     }
@@ -52,6 +94,7 @@ public class StrengthChecker {
      * @param minLength the minLength to set
      */
     public void setMinLength(int minLength) {
+        
         this.minLength = minLength;
     }
 
@@ -67,6 +110,13 @@ public class StrengthChecker {
      */
     public void setMaxLength(int maxLength) {
         this.maxLength = maxLength;
+    }
+
+    /**
+     * @return the feedback for in-depth explanation of why a password passed or failed.  Parse it yourself.
+     */
+    public String[][] getFeedback() {
+        return feedback;
     }
     
 }
--- Java244/src/PasswordChecker/TestClient.java
+++ Java244/src/PasswordChecker/TestClient.java
@@ -11,22 +11,31 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
- *
+ * I did not implement expanding the requirements beyond the original constructor string,
+ * mostly because it was added as a requirement after I had finished the day one requirements.
+ * 
  * @author noah.baertsch
  */
 public class TestClient {
     public static void main(String[] args){
         Scanner in = new Scanner(System.in);
-        StrengthChecker sc = new StrengthChecker("123%");
-        sc.setMaxLength(10);
-        sc.setMinLength(5);
+        
+        // enter sc params
+        System.out.print("Enter required characters, followed by [ENTER]: ");
+        StrengthChecker sc = new StrengthChecker(in.nextLine());
+        System.out.print("Enter max pass length, followed by [ENTER]: ");
+        sc.setMaxLength(in.nextInt());
+        System.out.print("Enter min pass length, followed by [ENTER]: ");
+        sc.setMinLength(in.nextInt());
         
         // Enter pass
         System.out.println("Required chars: " + Arrays.toString(sc.getReqChars()));
         System.out.println("Minimum length: " + sc.getMinLength());
         System.out.println("Maximum length: " + sc.getMaxLength());
         System.out.println("\nEnter pass: ");
-        String pass = in.nextLine();
+        String pass = in.next();
+        
+        System.out.println("Password = " + pass);
         
         // Test pass
         try{
@@ -35,9 +44,23 @@ public class TestClient {
             }else{
                 System.out.println("Password failed!");
             }
+            System.out.println();
+        
+            String[][] fb = sc.getFeedback();
+        
+            // this is hacky and for any real use should be considered broken
+            for(int i=0; i<(sc.getReqChars().length+2); i++){
+                for(int j=0; j<2; j++){
+                    System.out.print(fb[i][j]+"\t");
+                
+                }
+                System.out.println();
+            }
         } catch (ObjectMisconfiguredException ex) {
             System.out.println("ObjectMisconfiguredException | " + ex);
         }
+        
+        
         
         in.close();
     }
